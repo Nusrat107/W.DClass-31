475c6c718e8f860c1176f2663fccc879df1043fc
Nusrat Jahan <nusratjahanitbd1@gmail.com>
2025-01-29 22:52:39 +0600

    Class-31

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log ("Hello World");
+//    Addition
+    console.log ("Addition");
+    console.log ("50+60");
+    let rahim = 50;
+    let korim = 60;
+     addition = rahim+korim;
+    console.log(addition);
+
+    // Subtraction
+    console.log ("Subtraction");
+    console.log ("107-31");
+    let number1 = 107;
+    let number2 = 31;
+    sub = number1-number2;
+    console.log (sub);
+
+    // Multiplication
+    console.log ("Multiplication");
+    console.log ("331*803");
+    let digit1 = 331;
+    let digit2 = 803;
+    Mul = digit1*digit2;
+    console.log (Mul);
+
+    // Division
+    console.log ("Division");
+    console.log ("99/11");
+    let data1 = 99;
+    let data2 = 11;
+    division = data1/data2;
+    console.log (division);
